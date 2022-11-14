51da8b302f53882df89ada9b7bd165d422cf5592
Parabeac-Nest <admin@parabeac.com>
2022-11-14 14:35:23 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/teames/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.teames
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/teames_colors.g.dart
@@ -0,0 +1,16 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TeamesColors {
+
+static const Color fillIcon = Color(0xffffffff);
+
+static const Color duotone = Color(0xff7e869e);
+
+static const Color lineIcon = Color(0xffccd2e3);
+}
